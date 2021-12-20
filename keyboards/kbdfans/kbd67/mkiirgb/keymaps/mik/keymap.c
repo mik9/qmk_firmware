77f344c5b0800700d85f74ac81aff1e1cef1728f
Kyrylo Mikos <kiril.mik.os@gmail.com>
2021-12-20 10:56:03 +0200

    Better enabling rgb matrix.

--- keyboards/kbdfans/kbd67/mkiirgb/keymaps/mik/keymap.c
+++ keyboards/kbdfans/kbd67/mkiirgb/keymaps/mik/keymap.c
@@ -71,6 +71,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     break;
   case RGB_TOG:
     if (record->event.pressed) {
+        if (!rgb_matrix_is_enabled()) {
+            rgb_matrix_enable();
+        }
         if (last_rgb_matrix_val == 0) {
             last_rgb_matrix_val = rgb_matrix_get_val();
 
